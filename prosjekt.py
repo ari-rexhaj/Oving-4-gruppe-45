ec7c97e60b7a60a4483358bce0320a1ab2f34190
ari-rexhaj <arirexhajj@gmail.com>
2023-11-17 10:59:59 +0100

    mamma mia

--- prosjekt.py
+++ prosjekt.py
@@ -101,3 +101,129 @@ else:
 print("- - -")
 
 print(f"Den totale planteveksten er: {plantevekst(temperaturer)}")        #total plantgrowth
+
+# format: Navn ; Stasjon ; Tid(norsk normaltid) ; Snødybde ; Nedbør (døgn) ; Middeltemperatur (døgn) ; Gjennomsnittlig skydekke (døgn) ; Høyeste middelvind (døgn)
+
+location = __file__.replace("prosjekt.py","")
+
+file_data = open(f"{location}snoedybder_vaer_fem_stasjoner_dogn.csv_")
+list_data = file_data.readlines()
+list_data.pop(0)
+list_data.pop(len(list_data)-1)
+
+Processed_data = []
+
+def lagre_som_hashmap(list_data):
+    for data in list_data:
+        i = 0
+        temp_dict = {"Sted":"","Stasjon":"","Dato":"","Snødybde":0.0,"Middel temperatur":0.0,"Gjennomsnittlig skydekke":0.0,"Høyeste middelvind":0.0}
+
+        for value in data.split(";"):
+
+            if value == "-" or value == "-\n":
+                value = "ingen data"
+                continue
+
+            if i > 2:
+                value = value.replace(",",".")
+
+            match i:
+                case 0:
+                    temp_dict["Sted"] = value
+                case 1:
+                    temp_dict["Stasjon"] = value
+                case 2:
+                    temp_dict["Dato"] = value
+                case 3:
+                    temp_dict["Snødybde"] = float(value)
+                case 4:
+                    temp_dict["Middel temperatur"] = float(value)
+                case 5:
+                    temp_dict["Gjennomsnittlig skydekke"] = float(value)
+                case 6:
+                    temp_dict["Høyeste middelvind"] = float(value)
+            i += 1
+
+        Processed_data.append(temp_dict)
+    return Processed_data 
+
+
+def fordel_data_per_stasjon(dict_list:list[dict]):
+
+    SN12960_list = []
+    SN13420_list = []
+    SN10380_list = []
+    SN40420_list = []
+    SN69100_list = []
+
+    for data in dict_list:
+        match data["Stasjon"]:
+
+            case "SN12960":
+                SN12960_list.append(data)
+            case "SN13420":
+                SN13420_list.append(data)
+            case "SN10380":
+                SN10380_list.append(data)
+            case "SN40420":
+                SN40420_list.append(data)
+            case "SN69100":
+                SN69100_list.append(data)
+
+    return ((SN12960_list,SN13420_list,SN10380_list,SN40420_list,SN69100_list))
+
+def er_i_skisesong(Dato):
+    if int(Dato[3:5]) >= 10 or int(Dato[3:5]) <= 6:
+        return True
+    else:
+        return False     
+
+def finn_skifører(dict_list:list[dict]):
+
+    temp_list = []
+    finished_list = []
+
+    for data in dict_list:
+
+        if er_i_skisesong(data["Dato"]):
+            temp_list.append(data["Snødybde"])
+        else:
+            if len(temp_list) != 0:
+                finished_list.append(temp_list)
+            temp_list = []
+
+    if len(temp_list) != 0:
+        finished_list.append(temp_list)
+
+    return finished_list
+
+dict_list = lagre_som_hashmap(list_data)
+stasjon_data_list = fordel_data_per_stasjon(dict_list)
+
+skifører_list = []
+for stasjon in stasjon_data_list:
+    stasjon_skiføre = finn_skifører(stasjon)
+
+    temp_list = []
+
+    for value in stasjon_skiføre:
+        done = check(value,20)
+        if len(done) != 0:
+            temp_list.append(done)
+
+    skifører_list.append(temp_list)
+
+def Prep_trend_data(dict_list:list[dict]):
+
+    aar_list = [] #this shit dont make sense
+    skisesong_list = []
+
+    for data in dict_list:
+        if er_i_skisesong(data["Dato"]):
+            aar_list.append(int(data["Dato"][6:10]))
+            skisesong_list.append(data["Snødybde"])
+
+    return ((aar_list,skisesong_list))
+
+xy_list = Prep_trend_data(stasjon_data_list[0])
+
